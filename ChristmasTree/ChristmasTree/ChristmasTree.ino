12a50817643a609988d664f3d7c30c7efa33f6b4
Jason Springart <JasonSpringart@savance.com>
2018-12-28 10:04:54 -0500

    Initial Commit

---
+++ ChristmasTree/ChristmasTree/ChristmasTree.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ChristmasTree.ino
+ Created:	12/28/2018 10:00:54 AM
+ Author:	JasonSpringart
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
