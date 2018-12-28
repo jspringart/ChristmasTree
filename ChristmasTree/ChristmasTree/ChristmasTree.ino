6261d8fddf1c3b3c46dda2716afcea19bda64efe
Jason Springart <JasonSpringart@savance.com>
2018-12-28 12:02:02 -0500

    Initial Methods

--- ChristmasTree/ChristmasTree/ChristmasTree.ino
+++ ChristmasTree/ChristmasTree/ChristmasTree.ino
@@ -4,12 +4,45 @@
  Author:	JasonSpringart
 */
 
+// pin mapping
+int lightsLineA = 1;
+int lightsLineB = 2;
+int lightsLineEnable = 13;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
+	Serial.begin(9600);
 
+	pinMode(lightsLineA, OUTPUT);
+	pinMode(lightsLineB, OUTPUT);
+	pinMode(lightsLineEnable, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	checkSerial();
+}
+
+void checkSerial() {
+
+}
+
+void splitSerialData() {
+
+}
+
+void parseSerialData() {
+
+}
+
+void setLightPower() {
+
+}
+
+void setLightColor() {
+
+}
+
+void setLightBrightness() {
+
 }
