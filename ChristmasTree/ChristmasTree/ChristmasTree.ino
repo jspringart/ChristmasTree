4eaf2531a1d919dbccf664d99501af64b426ab71
Jason Springart <jspringart@gmail.com>
2019-12-17 18:21:10 -0500

    testing again

--- ChristmasTree/ChristmasTree/ChristmasTree.ino
+++ ChristmasTree/ChristmasTree/ChristmasTree.ino
@@ -4,6 +4,11 @@
  Author:	Jason Springart
 */
 
+int *p;
+
+volatile uint8_t *t;
+bool color = true;
+
 void setup() {
 	pinMode(5, OUTPUT);
 	pinMode(6, OUTPUT);
@@ -13,8 +18,26 @@ void setup() {
 	TCCR0B = 0b00000011;// prescaler 64
 	OCR0A = 50;//duty cycle for pin 6
 	OCR0B = 50;//duty cycle for pin 5
+	t = &OCR0A;
 }
 
+int counter = 0;
 void loop() {
-
+	counter++;
+	if (counter == 255) {
+		counter = 0;
+		*t = counter;
+		if (color)
+		{
+			t = &OCR0A;
+			color = false;
+		}
+		else
+		{
+			t = &OCR0B;
+			color = true;
+		}
+	}
+	
+	delayMicroseconds(500000);
 }
